e1c7b61ec53194404b22cf2da0a022c91757ed3a
ratchabala <ratchabala@gmail.com>
2021-03-15 22:35:26 +0530

    sree

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <body>
 <div id="clue">
 
-<button type="button" class="" data-toggle="modal" data-target="#ques1" id="q1"></button>
+<button type="button" class="butt" data-toggle="modal" data-target="#ques1" id="q1"></button>
 <div class="modal fade" id="ques1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -36,7 +36,7 @@
 </div>
 </div>
 
-<button type="button" class="" data-toggle="modal" data-target="#ques2" id="q2"></button><!--hidden-->
+<button type="button" class="butt" data-toggle="modal" data-target="#ques2" id="q2"></button><!--hidden-->
 <div class="modal fade" id="ques2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -59,7 +59,7 @@
 </div>
 </div>
 
-<button type="button" class="" data-toggle="modal" data-target="#ques3" id="q3"></button><!--hidden-->
+<button type="button" class="butt" data-toggle="modal" data-target="#ques3" id="q3"></button><!--hidden-->
 <div class="modal fade" id="ques3" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -82,7 +82,7 @@
 </div>
 </div>
 
-<button type="button" class="" data-toggle="modal" data-target="#ques4" id="q4"></button><!--hidden-->
+<button type="button" class="butt" data-toggle="modal" data-target="#ques4" id="q4"></button><!--hidden-->
 <div class="modal fade" id="ques4" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -105,7 +105,7 @@
 </div>
 </div>
 
-<button type="button" class="" data-toggle="modal" data-target="#ques5" id="q5"></button><!--hidden-->
+<button type="button" class="butt" data-toggle="modal" data-target="#ques5" id="q5"></button><!--hidden-->
 <div class="modal fade" id="ques5" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -128,7 +128,7 @@
 </div>
 </div>
 
-<button type="button" class="" data-toggle="modal" data-target="#ques6" id="q6"></button><!--hidden-->
+<button type="button" class="butt" data-toggle="modal" data-target="#ques6" id="q6"></button><!--hidden-->
 <div class="modal fade" id="ques6" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -151,7 +151,7 @@
 </div>
 </div>
 
-<button type="button" class="" data-toggle="modal" data-target="#ques7" id="q7"></button><!--hidden-->
+<button type="button" class="butt" data-toggle="modal" data-target="#ques7" id="q7"></button><!--hidden-->
 <div class="modal fade" id="ques7" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -163,6 +163,52 @@
       </div>
       <div class="modal-body">
         <div>Question7</div>
+        <textarea id="1"></textarea>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-success">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<button type="button" class="butt" data-toggle="modal" data-target="#ques8" id="q8"></button><!--hidden-->
+<div class="modal fade" id="ques8" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Question number</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div>Question8</div>
+        <textarea id="1"></textarea>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-success">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+
+<button type="button" class="butt" data-toggle="modal" data-target="#ques9" id="q9"></button><!--hidden-->
+<div class="modal fade" id="ques9" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Question number</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div>Question9</div>
         <textarea id="1"></textarea>
       </div>
       <div class="modal-footer">
