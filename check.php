6d094329da91cce62411b37a983409d1258803f2
bmspr1502 <57596428+bmspr1502@users.noreply.github.com>
2021-03-19 16:12:25 +0530

    don't consider this commit

--- check.php
+++ check.php
@@ -16,10 +16,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $sql1= "SELECT * FROM userdata WHERE kid='$kid'";
   $result= $con->query($sql1);
   $count = $result->num_rows;
-  if($count == 1){
+  if($count == 1)
+  {
     $_SESSION["kid"] = $kid;
     $_SESSION["phone"] = $phone;
-    echo "<script>window.location.href='round1.php'</script>";
+    header("location: round1.php");
   }
   else {
     $sql = "INSERT INTO userdata (kid, name, email, phone, start) VALUES ('$kid', '$name', '$email', '$phone', CURRENT_TIMESTAMP())";
@@ -28,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       echo "New user inserted";
       $_SESSION["kid"] = $kid;
       $_SESSION["phone"] = $phone;
-      echo "<script>window.location.href='round1.php'</script>";
+      header("location: round1.php");
     } else {
       echo "Error: " . $sql . "<br>" . $con->error;
     }
--- end.php
+++ end.php
@@ -3,8 +3,8 @@
   if(isset($_POST['kid'])) {
     include('functions/DB.php');
     $kid = $_POST["kid"];
-    $sql2 = "UPDATE userdata SET endtime = CURRENT_TIMESTAMP(), round1=1 WHERE kid = '$kid'";
-    if ($con->query($sql2)) {
+    $sql2 = "UPDATE userdata SET endtime = CURRENT_TIMESTAMP() WHERE kid = '$kid'";
+    if (isset($con->query($sql2))) {
       echo "Done";
       unset($_SESSION['kid']);
       unset($_SESSION['phone']);
