e9a7d8bf911a011ced81a3f40f3a84e9dcafa780
hemagunnala <gunnalahema14134@gmail.com>
2021-03-29 13:50:20 +0530

    added questions

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Decryptit | Round-1</title>
+  <title>Decryptit | Round-2</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -18,13 +18,18 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Question number</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Question</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <div>Write a program for error correction in digital communication</div>
+        <div>Decrypt the following message 😉
+
+<br>Fictional border ordinary boredom
+<br>Elephant fox programming genre 
+<br>Robotics mini enigmatic son
+<br>Classy experience</div>
         <textarea id="1"></textarea>
       </div>
       <div class="modal-footer">
@@ -41,13 +46,15 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Question number</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Question</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <div>Write a program to exchange all odd bits with even bits.(Note: input must be binary)</div>
+        <div>Decrypt the following message 😊
+<br>12-5-1-18-14
+</div>
         <textarea id="1"></textarea>
       </div>
       <div class="modal-footer">
@@ -64,17 +71,15 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Question number</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Question</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <div>Write a program to print
-            <div style="margin-left:50%">*</div>
-            <div style="margin-left:43%">* A *</div>
-            <div style="margin-left:37%">* A * B *</div>
-            <div style="margin-left:32%">* A * B * C *</div>
+        <div>
+        Decrypt the following message 😭
+        <img src="images/3rd question img.png" alt="IMAGE">
         </div>
         <textarea id="1"></textarea>
       </div>
@@ -92,36 +97,15 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Question number</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Question</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <div>Take any two numbers for ex: 18 12
-        <table>
-          <tr>
-            <td>18</td> 
-            <td>12</td>
-          </tr>
-          <tr>
-            <td>6</td> 
-            <td>12</td>
-          </tr>
-          <tr>
-            <td>12</td> 
-            <td>6</td>
-          </tr>
-          <tr>
-            <td>6</td> 
-            <td>6</td>
-          </tr>
-          <tr>
-            <td>0</td> 
-            <td>6</td>
-          </tr>
-        </table>
-          take input from user and implement the opeation done above ,display only the final result.
+        <div>Decrypt the following message 😎
+<br>UM UZNKXY
+<br>Hint:The number of the most valuable and the least valued thing in a million
         </div>
         <textarea id="1"></textarea>
       </div>
@@ -139,13 +123,15 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Question number</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Question</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body">
-        <div>91% of the girls aged 10-18 years have experienced all types of harasement.Studies show that due to social expression and fear of retaliation the reported number is far leass than the real number.</div>
+      <div class="modal-body ">
+        <div>Decrypt: 67-72-2-38-92 🙄
+<br>Hint:“No matter what he does, every person on earth plays a central role in the history of the world. And normally he doesn't know it.”
+</div>
         <textarea id="1"></textarea>
       </div>
       <div class="modal-footer">
@@ -158,18 +144,20 @@
 </div>
 
 <button type="button" class="butt" data-toggle="modal" data-target="#ques6" id="q6"></button><!--hidden-->
-<div class="modal fade" id="ques6" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="ques6" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="width:900px">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Question number</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Question</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <div>Our teams are over run with emergencies and urgent requests.This means we have no time to innovate or to get the best from staff.If this continues, our competion will push us out of the market.</div>
-        <textarea id="1"></textarea>
+        <div>Decrypt the following message 😶
+        <img src="images/6th question img1.png" alt="img">
+        <br>key:
+        <img src="images/6th question img2.png" alt="img">
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
@@ -185,14 +173,14 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Question number</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Question</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <div>The boarding protocols used by XYZ airlines should aim to get each flights passengers aboard the plane quickly and efficiently so that the plane can take off as soon as possible.</div>
-        <textarea id="1"></textarea>
+        <div>Decrypt the following message 😴
+            <br>LRRENOAFM
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
@@ -208,37 +196,14 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Question number</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Question</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        <div>All smart devices,uses our passwords,voice,touch ID etc and all of them are synced.How can we improve the security of data shared.</div>
-        <textarea id="1"></textarea>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-success">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-
-<button type="button" class="butt" data-toggle="modal" data-target="#ques9" id="q9"></button><!--hidden-->
-<div class="modal fade" id="ques9" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Question number</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <div>Overfill has been a serious problem facing our city waste facilities for the last decade.By some estimations,our city dumps are an average,30% above capacity in unsanitary,unsafe & unwise position for our city to be in.</div>
-        <textarea id="1"></textarea>
+      Attend this question after completing all other 7 questions:
+From the previously decrypted chunks of messages formulate the entire statement🙏     
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
