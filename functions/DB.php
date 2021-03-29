7b3b53940a347d79016421b656561d91261f9ea3
bmspr1502 <57596428+bmspr1502@users.noreply.github.com>
2021-03-30 01:00:54 +0530

    Update DB.php

--- functions/DB.php
+++ functions/DB.php
@@ -4,6 +4,12 @@ $host = "localhost";
 $user = "u672681888_root";
 $password = "3d#MobyE";
 $database = "u672681888_decryptit";
+*/
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "decrypit";
 
 $con = new mysqli($host, $user, $password, $database);
 
