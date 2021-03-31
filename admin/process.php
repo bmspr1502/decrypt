9dc9f2c75ba96d76eb4385de1e1c1bfa8bf666ff
bmspr1502 <57596428+bmspr1502@users.noreply.github.com>
2021-04-01 01:55:38 +0530

    Update process.php

--- admin/process.php
+++ admin/process.php
@@ -2,8 +2,7 @@
 if(isset($_POST['kid'])){
     include "../functions/DB.php";
     $kid = $con->real_escape_string($_POST['kid']);
-    $score = $con->real_escape_string($_POST['score']);
-    $score = number_format($score);
+    $score = $_POST['score'];
     $selected = $con->real_escape_string($_POST['selected']);
     $query1 = "UPDATE round1 SET totscore = $score, selected = $selected WHERE kid = '$kid';";
     if($con->query($query1)){
